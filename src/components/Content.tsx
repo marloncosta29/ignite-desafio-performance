32a88bfd8a5883c5f7e71c28dc241d0c43966ab7
marloncosta29 <marlon.29@outlook.com>
2021-08-18 22:15:02 -0300

    colocado hooks de memo e callback e adicionado o react-virtualized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,9 +1,16 @@
 import { MovieCard } from "./MovieCard";
-
+import {
+  AutoSizer,
+  List,
+  ListRowRenderer,
+  ColumnSizer,
+  Grid,
+  GridCellRenderer,
+} from "react-virtualized";
 interface ContentProps {
   selectedGenre: {
     id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
     title: string;
   };
 
@@ -20,19 +27,43 @@ interface ContentProps {
 }
 
 export function Content({ selectedGenre, movies }: ContentProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <MovieCard
+          key={movies[index].imdbID}
+          title={movies[index].Title}
+          poster={movies[index].Poster}
+          runtime={movies[index].Runtime}
+          rating={movies[index].Ratings[0].Value}
+        />
+      </div>
+    );
+  };
+
   return (
     <div className="container">
       <header>
-        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
       </header>
 
-      <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
-        </div>
+      <main style={{ height: "100%" }}>
+        <AutoSizer>
+          {({ width, height }) => {
+            return (
+              <List
+                width={width}
+                height={height}
+                rowRenderer={rowRenderer}
+                rowCount={movies.length}
+                rowHeight={400}
+              />
+            );
+          }}
+        </AutoSizer>
       </main>
     </div>
-  )
+  );
 }
